dd59591be699406070045a74a358c37cd0552a92
quantumduck <quantum.duck@hotmail.com>
2017-03-28 14:38:30 -0400

    exercise 8

---
+++ x8.rb
@@ -0,0 +1,16 @@
+expenses = [250, 7.95, 30.95, 16.50]
+
+class Array
+  def money_add
+    total = 0
+    self.each do |amt|
+      total += (100.00 * amt).to_i
+      # puts total
+    end
+    total.to_f / 100.00
+  end
+end
+
+puts expenses.money_add
+
+puts [expenses.money_add].money_add

fbeea0aeb110815f78141b40028a70df0bb8ae0a
quantumduck <quantum.duck@hotmail.com>
2017-03-28 13:27:27 -0400

    fix for exercize 5

--- x5.rb
+++ x5.rb
@@ -1,10 +1,14 @@
 def greet_backwards(name)
-  greeting = "Hello, "
-  while (name.length > 0)
-    backname << name.pop
+  pair_index = 1
+  while (pair_index <= (name.length / 2))
+    # swap letters
+    temp = name[pair_index-1]
+    name[pair_index-1] = name[-pair_index]
+    name[-pair_index] = temp
+    pair_index += 1
   end
-  greeting += "! Welcome home."
-  return backname
+  greeting = "Hello #{name}! Welcome home."
+  return greeting
 end
 
 people = ["Bob", "Shirly", "Sue", "Andy"]

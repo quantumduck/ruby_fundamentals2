034b93f88f49a2c0a5aec7f3db50df600b8c747a
quantumduck <quantum.duck@hotmail.com>
2017-03-28 15:22:25 -0400

    up to part 4

--- exercise10.rb
+++ exercise10.rb
@@ -1,9 +1,11 @@
+puts '#1.'
 students = {
   :cohort1 => 34,
   :cohort2 => 42,
   :cohort3 => 22
 }
 
+puts'#2.'
 class Hash
   def print_hash
     self.each do |key, item|
@@ -13,3 +15,9 @@ class Hash
 end
 
 students.print_hash
+
+puts '#3.'
+students.store(:cohort4, 43)
+
+puts '#4.'
+puts students.keys
